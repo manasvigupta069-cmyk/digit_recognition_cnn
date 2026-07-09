f0dfbd5b31729d8c257a0a4ea598f40965aa3cd5
manasvigupta069-cmyk <manasvigupta069@gmail.com>
2026-07-09 22:06:07 +0530

    Update cnn_app.py

--- cnn_app.py
+++ cnn_app.py
@@ -10,11 +10,10 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 uploaded_file = st.file_uploader("Choose an image...")
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('L')  # Convert to grayscale
-        image = ImageOps.invert(image)  # Invert to match MNIST format
-        image = image.resize((28, 28))
-        img_array = np.array(image).reshape(-1, 28, 28, 1) / 255.0
-        st.image(image, caption="Uploaded Image", width=150)
-
-        prediction = model.predict(img_array)
-        st.success(f"Predicted Digit: {np.argmax(prediction)}")
+    image = ImageOps.invert(image)  # Invert to match MNIST format
+    image = image.resize((28, 28))
+    img_array = np.array(image).reshape(-1, 28, 28, 1) / 255.0
+    st.image(image, caption="Uploaded Image", width=150)
+    prediction = model.predict(img_array)
+    st.success(f"Predicted Digit: {np.argmax(prediction)}")
         
